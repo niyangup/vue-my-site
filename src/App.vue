d13ad0a2da9ff3884502d66fb752304526a5f862
poper <poper@wangxutech.com>
2022-03-07 22:59:49 +0800

    empty组件

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,21 @@
 <template>
   <div id="app">
     <pager :total="total" :current="current" @pageChange="handlePageChange"></pager>
+    <div class="container">
+      <empty/>
+    </div>
   </div>
 </template>
 
 <script>
 
 import Pager from "@/components/Pager";
+import Empty from "@/components/Empty";
 
 export default {
   name: 'App',
   components: {
+    Empty,
     Pager,
   },
   data() {
@@ -29,5 +34,12 @@ export default {
 
 <style>
 #app {
+}
+
+.container {
+  position: relative;
+  width: 300px;
+  height: 300px;
+  border: 3px solid #dedede;
 }
 </style>
---
+++ src/components/Empty.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="empty-container">
+    <Icon type="empty"></Icon>
+    <p>{{ text }}</p>
+  </div>
+</template>
+
+<script>
+import Icon from "@/components/Icon";
+
+export default {
+  name: "Empty",
+  components: {Icon},
+  props: {
+    text: {
+      type: String,
+      default: '无数据'
+    },
+  },
+}
+</script>
+
+<style scoped lang="less">
+@import url(~@/style/mixin.less);
+@import url(~@/style/var.less);
+
+.empty-container {
+  .self-center();
+
+  .icon-container {
+    font-size: 72px;
+    color: @gray;
+  }
+
+  p {
+    color: @gray;
+    text-align: center;
+  }
+}
+
+
+</style>
