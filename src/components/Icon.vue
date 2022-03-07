b3a5e9f753126c568bd563654f26f7ed36bab6ee
poper <poper@wangxutech.com>
2022-03-07 17:42:44 +0800

    素材上传

--- src/components/Icon.vue
+++ src/components/Icon.vue
@@ -1,33 +1,48 @@
 <template>
-  <div class="icon-container">
-    <i class="iconfont" :class="fontClass"></i>
-  </div>
+  <i class="iconfont icon-container" :class="fontClass"></i>
 </template>
 
 <script>
-const data = {
-  empty: "icon-empty",
-  up: 'icon-xiangshang',
-  down: 'icon-xiangxia',
-  home: 'icon-home',
-}
+const classMap = {
+  home: "iconzhuye",
+  success: "iconzhengque",
+  error: "iconcuowu",
+  close: "iconguanbi",
+  warn: "iconjinggao",
+  info: "iconxinxi",
+  blog: "iconblog",
+  code: "iconcode",
+  about: "iconset_about_hov",
+  weixin: "iconweixin",
+  mail: "iconemail",
+  github: "icongithub",
+  qq: "iconsign_qq",
+  arrowUp: "iconiconfonticonfonti2copy",
+  arrowDown: "iconiconfonticonfonti2",
+  empty: "iconempty",
+  chat: "iconliuyan",
+};
+export const types = Object.keys(classMap);
 export default {
-  name: "Icon",
   props: {
     type: {
       type: String,
-      default: 'icon-empty'
+      required: true,
     },
   },
   computed: {
     fontClass() {
-      return data[this.type]
-    }
+      return classMap[this.type];
+    },
   },
-}
+};
 </script>
 
 <style scoped>
-@import "//at.alicdn.com/t/font_3002025_bwqdx5f9wan.css";
+@import "//at.alicdn.com/t/font_2164449_nalfgtq7il.css";
 
+.iconfont {
+  color: inherit;
+  font-size: inherit;
+}
 </style>
