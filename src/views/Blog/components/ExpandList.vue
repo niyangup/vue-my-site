a86b2ec1635814e0a4adc5348dc606c97486075d
poper <poper@wangxutech.com>
2022-03-11 11:10:57 +0800

    组件递归,通过name自己调用自己

---
+++ src/views/Blog/components/ExpandList.vue
@@ -0,0 +1,64 @@
+<template>
+
+  <div class="expand-list-container">
+    <ul>
+      <li v-for="(item,i) in list " :key="i" @click="handleClick(item)">
+        <span :class="{active:item.isSelect}">{{ item.name }}</span>
+        <ExpandList :list="item.children" @select="handleClick(item)"></ExpandList>
+      </li>
+    </ul>
+
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: "ExpandList",
+  props: {
+    /**
+     * {
+     *   name:'显示的问题',
+     *   isSelect:true //是否选中
+     *   children:[{name:'子级节点'},isSelect:false,children:[]]
+     * }
+     */
+    list: {
+      type: Array,
+      default: () => []
+    },
+  },
+  methods: {
+    handleClick(item) {
+      this.$emit('select', item)
+    }
+  },
+}
+</script>
+
+<style scoped lang="less">
+@import "~@/style/var.less";
+
+.expand-list-container {
+  ul {
+    list-style: none;
+    padding: 0;
+  }
+
+  .expand-list-container {
+    margin: 1em;
+  }
+
+  li {
+    min-height: 40px;
+    line-height: 40px;
+    cursor: pointer;
+
+    .active {
+      font-weight: bold;
+      color: @warn;
+    }
+  }
+}
+
+</style>
